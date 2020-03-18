55ae5db53b22b44f056794195fdb350f9631c759
Hassan James <59996947+hassan9174@users.noreply.github.com>
2020-03-18 14:44:42 +0700

    Update tor.py

--- tor.py
+++ tor.py
@@ -1,21 +1,2 @@
-import requesocks
-import ipaddress
-
-
-start_ip = ipaddress.IPv4Address('5.5.5.5')
-end_ip = ipaddress.IPv4Address('5.5.10.5')
-for ip_int in range(int(start_ip), int(end_ip)):
-    print(ipaddress.IPv4Address(ip_int))
-    print "(+) Sending request with requesocks..."
-    session = requesocks.session()
-    session.proxies = {'http': 'socks5://127.0.0.1:9050',
-                       'https': 'socks5://127.0.0.1:9050'}
-    r = session.get(ipaddress.IPv4Address(ip_int))
-
-
-def main():
-    print "Running tests..."
-
-
-if __name__ == "__main__":
-    main()
+skype id is this
+live:.cid.4456d604342e894f
